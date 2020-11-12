8a2f6a504200c6f1bd5ad3a36729a286845f1dc6
jakedebest <jwinnett@tutanota.com>
2020-11-12 14:14:53 +0000

    updating db

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const knex = require('knex')({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : 'postgresql-trapezoidal-55023',
       user : 'postgres',
       password : 'password',
       database : 'smartbrain'
